e9881ee27282486db89c02fc56620ec80bf33988
Henry <89488963+Old-Lai@users.noreply.github.com>
2026-03-07 20:19:14 -0800

    encapsulat app.ts into a buildApp function convert server into a function call citing buildApp better utilizing fastify api

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,25 +1,24 @@
 import Fastify, { FastifyInstance } from "fastify";
 import cors from "@fastify/cors";
 import servicesRoutes from "./routes/services";
-import fetchServiceRoutes from "./routes/fetchServices"
+import fetchServiceRoutes from "./routes/fetchServices";
 import { loadServices } from "./lib/serviceStore";
 
-const app = Fastify({ logger: true });
+export async function buildApp() {
+  const app = Fastify({ logger: true });
 
-async function loadService() {
   await loadServices();
+
+  // Enable CORS if needed
+  app.register(cors, { origin: "*" });
+
+  // Routes
+  app.register(apiRoutes, { prefix: "/api" });
+
+  return app;
 }
-loadService();
 
-// Enable CORS if needed
-app.register(cors, { origin: "*" });
-
-// Routes
 async function apiRoutes(api: FastifyInstance) {
   api.register(servicesRoutes, { prefix: "/services" });
-  api.register(fetchServiceRoutes, { prefix: "/update" })
+  api.register(fetchServiceRoutes, { prefix: "/update" });
 }
-
-app.register(apiRoutes, { prefix: "/api" });
-
-export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,16 @@
-import app from "./app";
+import { buildApp } from "./app";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen({ port: Number(PORT), host: "0.0.0.0" })
-  .then(() => console.log(`API running on port ${PORT}`))
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
+const start = async () => {
+  const app = await buildApp();
+  app
+    .listen({ port: Number(PORT), host: "0.0.0.0" })
+    .then(() => console.log(`API running on port ${PORT}`))
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
+};
+
+start();
