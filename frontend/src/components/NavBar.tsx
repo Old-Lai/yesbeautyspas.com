5f42f3e0a0c4c997e7f61112931a18a5c4024f62
Henry <89488963+Old-Lai@users.noreply.github.com>
2026-03-12 15:11:57 -0700

    removed default button style add basic navigation text

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,47 +1,66 @@
-type MENU_CATEGORY = {
-  name: string;
-  subCategory?: string[];
-};
-
 export default function NavBar() {
-  const MENU_CATEGORY: MENU_CATEGORY[] = [
-    {
-      name: "Facial",
-      subCategory: [
-        "Signiture Facial Treatments",
-        "Luxury Facial Treatments",
-        "Advanced Facial Treatments",
-        "Add-on & Enhancements",
-      ],
-    },
-    {
-      name: "Lash & Brow",
-    },
-    {
-      name: "Massage Therapy",
-    },
-    {
-      name: "Head Spa",
-    },
-  ];
   return (
     <div className="h-full w-full">
-      <ul className="">
+      <ul>
         {MENU_CATEGORY.map((category) => {
           if (category.subCategory)
             return (
-              <li>
-                {category.name}
+              <li key={category.name}>
+                <button className={`${navButtonStyles}`}>
+                  {category.name}
+                </button>
+
                 <ul>
                   {category.subCategory.map((subCategoryName) => {
-                    return <li>{subCategoryName}</li>;
+                    return (
+                      <li key={subCategoryName}>
+                        <button
+                          className={`${navButtonStyles} ${subNavButtonStyles}`}
+                        >
+                          {subCategoryName}
+                        </button>
+                      </li>
+                    );
                   })}
                 </ul>
               </li>
             );
-          return <li>{category.name}</li>;
+          return (
+            <li key={category.name}>
+              <button className={`${navButtonStyles}`}>{category.name}</button>
+            </li>
+          );
         })}
       </ul>
     </div>
   );
 }
+
+type MENU_CATEGORY = {
+  name: string;
+  subCategory?: string[];
+};
+
+const MENU_CATEGORY: MENU_CATEGORY[] = [
+  {
+    name: "Facial",
+    subCategory: [
+      "Signiture Facial Treatments",
+      "Luxury Facial Treatments",
+      "Advanced Facial Treatments",
+      "Add-on & Enhancements",
+    ],
+  },
+  {
+    name: "Lash & Brow",
+  },
+  {
+    name: "Massage Therapy",
+  },
+  {
+    name: "Head Spa",
+  },
+];
+
+const navButtonStyles = "p-3";
+const subNavButtonStyles = "hidden";
